91711df701aa9224051c70805c2edade4a276803
kingbbode <kingbbode@gmail.com>
2018-12-02 18:19:36 +0900

    [theft-domain] 도둑질해서 저장될 내용은 세일 정보로 한다.

---
+++ theft-domain/src/main/java/com/github/kingbbode/restdocs/domain/sale/SaleItem.java
@@ -0,0 +1,26 @@
+package com.github.kingbbode.restdocs.domain.sale;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SaleItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String price;
+}
---
+++ theft-domain/src/main/java/com/github/kingbbode/restdocs/domain/sale/SaleItemRepository.java
@@ -0,0 +1,6 @@
+package com.github.kingbbode.restdocs.domain.sale;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
+}
